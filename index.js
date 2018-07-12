5d7b73bb31633db2d82f7945e2a7ebefcce7428f
Lydia Tan <lydia.sydia@gmail.com>
2018-07-12 15:29:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function printBadges(array){
+  for(var i = 0; i < array.length; i++){
+   console.log("Welcome " + array[i] + "!" + " You are employee #" + i)
+ }
+ return array
+}
+
+var heads = false;
+var tails = 0;
+function tailsNeverFails(){
+  while(heads === false){
+    var flip = Math.random(1, 2)
+    if(flip === 1){
+      heads++
+    }
+  }
+}
